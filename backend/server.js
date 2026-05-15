1402f436a5841edf25275c31b01468649b3718c0
iqrase <iqrasiddiqui@gmail.com>
2026-05-15 13:56:57 +0500

    fix: cors update for vercel

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import{connectDB } from './config/db.js'
-
+import { connectDB } from './config/db.js'
 
 import userRouter from './routes/userRoute.js'
 import taskRouter from './routes/taskRoute.js'
@@ -10,25 +9,31 @@ import authRoutes from './routes/auth.js'
 
 const app = express();
 const port = process.env.PORT || 4001;
-app.use(cors({ origin: 'http://localhost:5173' }))
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://task-flow-eight-taupe.vercel.app'
+  ],
+  credentials: true
+}))
 
 // MIDDLEWARE
-app.use(cors(({ origin: 'http://localhost:5173' })));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//DB CONNECT
+// DB CONNECT
 connectDB();
 
-//ROUTES
+// ROUTES
 app.use("/api/user", userRouter);
 app.use("/api/tasks", taskRouter);
 app.use('/api/auth', authRoutes);
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('API WORKING');
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`)
 })
